b44a160b07eb62f74a0b9d90a0054f4cbed99c2c
minium2 <e.panteleev@g.nsu.ru>
2023-04-05 00:25:59 +0700

    refactor Page.cpp

--- src/gc/containers/Page.cpp
+++ src/gc/containers/Page.cpp
@@ -8,7 +8,8 @@ void *Page::alloc(std::size_t size) noexcept {
     void* page = mmap(nullptr, size, PROT_READ | PROT_WRITE | PROT_EXEC,
                       MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
     if (page == MAP_FAILED) {
-        std::fprintf(stderr, "mmap failed: size=%zul\n", size);
+        std::fprintf(stderr, "mmap failed: size=%zul ", size);
+        perror("cause: ");
         std::terminate();
     }
     return page;
@@ -17,8 +18,8 @@ void *Page::alloc(std::size_t size) noexcept {
 void *Page::realloc(void *old_addr, std::size_t old_size, std::size_t new_size) noexcept {
     void *p = mremap(old_addr, old_size, new_size, MREMAP_MAYMOVE);
     if (p == MAP_FAILED) {
-        std::fprintf(stderr, "mremap failed: old_addr=%p, old_size=%zu, new_size=%zu", old_addr, old_size, new_size);
-        perror(" cause: ");
+        std::fprintf(stderr, "mremap failed: old_addr=%p, old_size=%zu, new_size=%zu ", old_addr, old_size, new_size);
+        perror("cause: ");
         std::terminate();
     }
     return p;
@@ -27,7 +28,8 @@ void *Page::realloc(void *old_addr, std::size_t old_size, std::size_t new_size) 
 void Page::free(void *old_addr, std::size_t size) noexcept {
     int ret = munmap(old_addr, size);
     if (ret == -1) {
-        std::fprintf(stderr, "munmap failed: old_addr=%p, old_size=%zul\n", old_addr, size);
+        std::fprintf(stderr, "munmap failed: old_addr=%p, old_size=%zul ", old_addr, size);
+        perror("cause: ");
         std::terminate();
     }
 }
